a1f2f626014743be3210a2d200d02a21aaaaf421
John Higgins <johnhiggins@outlook.ie>
2018-09-12 20:50:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine (line) {
   var newArray = []
   
   for (let i = 0; line.length > i; i++) {
-    newArray.push ('${i + 1}. ${line[i]}')
+    newArray.push('${i + 1}. ${line[i]}')
   }
   
   return String ('The line is currently: ${newArray.join(', ')}')
